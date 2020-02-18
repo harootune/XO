ff9c021f61301f5e19a426d2ff6f8ebefba51fe2
Derek Harootune Otis <dharootuneotis@gmail.com>
2020-02-17 23:44:09 -0600

    +Prototyped maintaining a game state across iterations of a while loop +Prototyped checking for win conditions +Prototyped formatting and input methods

--- xo.c
+++ xo.c
@@ -6,20 +6,73 @@
 int main()
 {
 	bool game_over;
+	char game_grid[3];
 	char response;
 	
+	game_grid[0] = '-';
+	game_grid[1] = '-';
+	game_grid[2] = '-';
+
 	game_over = false;
 
 	while (!(game_over))
 	{
-		printf("%s\n", "Would you like to end the game? (y/n):");
-		response = getchar();
+		printf("%s\n", "Game state:");
+		printf("%d %d %d\n", 1, 2, 3);
+		printf("%c %c %c\n", game_grid[0], game_grid[1], game_grid[2]);
 
-		if (response == 'y' || response == 'Y')
+		printf("%s\n", "Please select a space to fill (1-3):");
+		response = getchar();
+		getchar();
+		
+		switch(response)
+		{
+			case '1':
+				if (game_grid[0] == '-')
+				{
+					game_grid[0] = 'X';
+				}
+				else
+				{
+					printf("\n%s\n", "Space already filled, try again");
+				};
+				break;
+			case '2':
+				if (game_grid[1] == '-')
+				{
+					game_grid[1] = 'X';
+				}
+				else
+				{
+					printf("\n%s\n", "Space already filled, try again");
+				};
+				break;
+			case '3':
+				if (game_grid[2] == '-')
+				{
+					game_grid[2] = 'X';
+				}
+				else
+				{
+					printf("\n%s\n", "Space already filled, try again");
+				};
+				break;
+			default:
+				printf("%s\n", "Not a valid input, try again.");		
+		};
+
+		if (game_grid[0] == 'X' && game_grid[1] == 'X' && game_grid[2] == 'X')
 		{
 			game_over = true;
 		};
 	};
 
+	printf("%s\n", "Game state:");
+	printf("%d %d %d\n", 1, 2, 3);
+	printf("%c %c %c\n", game_grid[0], game_grid[1], game_grid[2]);
+	printf("%s\n", "Game Over!");
+	
 	return 0;
 };
+
+
