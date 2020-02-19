c8b35b8670b026256e8fd424e1c0a73a56ad3bff
Derek Harootune Otis <dharootuneotis@gmail.com>
2020-02-18 21:53:47 -0600

    Additions
    +3 spaces -> 3x3 grid!
    +2 players!
    +alternating turns!
    +custom player symbols!
    
    Bugs
    
    *Inputting an alphabet character into the coordinate prompt causes some
    kind of infinite loop to occur, where the prompt is repeated endlessly
    and constantly fails, but I can't figure out why
    
    TODO
    
    >Win conditions

--- xo.c
+++ xo.c
@@ -3,76 +3,127 @@
 #include <stdbool.h>
 #include <time.h>
 
+void draw_board(char game_grid[][3])
+{
+	printf("%s\n", "Game state:");
+	printf(" %d %d %d\n", 1, 2, 3);
+	for (int i=0; i<3; i++)
+	{
+		printf("%d", i+1);
+
+		for (int j=0; j<3; j++)
+		{
+			printf(" %c", game_grid[i][j]);
+		};
+
+		printf("%c", '\n');
+	};
+};
+
+
+int check_space(char game_grid[][3], int res_row, int res_column)
+{
+	char space;
+	
+	space = game_grid[res_row-1][res_column-1];
+
+	if (space == '-')
+	{
+		return 0;
+	}
+	else
+	{
+		printf("%s\n", "Space filled, please try again.");
+		return 1;
+	};
+
+	return 1;
+};
+
+
 int main()
 {
 	bool game_over;
-	char game_grid[3];
-	char response;
+	char game_grid[3][3];
+	char player_1;
+	char player_2;
+	char current_player;
+	int res_row;
+	int res_column;
+	int space_status;
+	int turn_count;
 	
-	game_grid[0] = '-';
-	game_grid[1] = '-';
-	game_grid[2] = '-';
-
-	game_over = false;
-
-	while (!(game_over))
+	for (int i=0; i<3; i++)
 	{
-		printf("%s\n", "Game state:");
-		printf("%d %d %d\n", 1, 2, 3);
-		printf("%c %c %c\n", game_grid[0], game_grid[1], game_grid[2]);
-
-		printf("%s\n", "Please select a space to fill (1-3):");
-		response = getchar();
-		getchar();
-		
-		switch(response)
+		for (int j=0; j<3; j++)
 		{
-			case '1':
-				if (game_grid[0] == '-')
-				{
-					game_grid[0] = 'X';
-				}
-				else
-				{
-					printf("\n%s\n", "Space already filled, try again");
-				};
-				break;
-			case '2':
-				if (game_grid[1] == '-')
-				{
-					game_grid[1] = 'X';
-				}
-				else
-				{
-					printf("\n%s\n", "Space already filled, try again");
-				};
-				break;
-			case '3':
-				if (game_grid[2] == '-')
-				{
-					game_grid[2] = 'X';
-				}
-				else
-				{
-					printf("\n%s\n", "Space already filled, try again");
-				};
-				break;
-			default:
-				printf("%s\n", "Not a valid input, try again.");		
-		};
-
-		if (game_grid[0] == 'X' && game_grid[1] == 'X' && game_grid[2] == 'X')
-		{
-			game_over = true;
+			game_grid[i][j] = '-';
 		};
 	};
 
-	printf("%s\n", "Game state:");
-	printf("%d %d %d\n", 1, 2, 3);
-	printf("%c %c %c\n", game_grid[0], game_grid[1], game_grid[2]);
-	printf("%s\n", "Game Over!");
+	game_over = false;
+	turn_count = 0;
+	res_row = 0;
+	res_row = 0;
+	
+	printf("%s", "Player 1, please input your symbol: ");
+	player_1 = getchar();
+	getchar();
+	printf("%s", "Player 2, please input your symbol: ");
+	player_2 = getchar();
+	getchar();
+	
+	current_player = player_1;
+
+	while (!(game_over))
+	{
+		draw_board(game_grid);
+		
+		printf("%c, %s\n", current_player, "Please input the coordinates of the space you'd like to fill (Format - row, column):");
+		scanf("%d, %d", &res_row, &res_column);
+		if (res_row < 1 ||
+		    res_row > 3 ||
+		    res_column < 1 ||
+		    res_column > 3)
+		{
+			printf("%s\n", "Invalid input, try again");
+			continue;
+		};
+
+
+		space_status = check_space(game_grid, res_row, res_column);	
+
+		if (!(space_status))
+		{
+			turn_count += 1;
+			game_grid[res_row-1][res_column-1] = current_player;
+			
+			if (current_player == player_1)
+			{
+				current_player = player_2;
+			}
+			else if (current_player == player_2)
+			{
+				current_player = player_1;
+			}
+			else
+			{
+				printf("%s", "LINE 110 FUCKED");
+			};	
+
+			if (turn_count == 9)
+			{
+				game_over = true;
+			};
+		};
+	};
+
+	draw_board(game_grid);
+
+	printf("\n%s\n", "Game Over!");
 	
 	return 0;
 };
+
 
 
