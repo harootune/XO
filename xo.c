153a10f8d07dbd21c5e1829a2cfd6e149a0f68be
Derek Harootune Otis <dharootuneotis@gmail.com>
2020-02-19 21:29:00 -0600

    Additions
    +Win conditions added
    +Improved output formatting
    +Fixed input bug
    
    Bugs
    
    *None currently known
    
    Todo
    
    >Clean Code
    >Comment Code
    >Track wins and losses during session
    >Offer restart option

--- xo.c
+++ xo.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include <time.h>
+#include <ctype.h>
 
 void draw_board(char game_grid[][3])
 {
-	printf("%s\n", "Game state:");
+	printf("\n%s\n", "Game state:");
 	printf(" %d %d %d\n", 1, 2, 3);
 	for (int i=0; i<3; i++)
 	{
@@ -21,36 +20,69 @@ void draw_board(char game_grid[][3])
 };
 
 
-int check_space(char game_grid[][3], int res_row, int res_column)
+bool check_space(char game_grid[][3], int res_row, int res_column)
 {
 	char space;
 	
-	space = game_grid[res_row-1][res_column-1];
+	space = game_grid[res_row][res_column];
 
 	if (space == '-')
 	{
-		return 0;
+		return true;
 	}
 	else
 	{
-		printf("%s\n", "Space filled, please try again.");
-		return 1;
+		return false;
 	};
 
-	return 1;
+	return false;
 };
+
+
+bool check_win(char game_grid[][3], int res_row, int res_column, char current_player)
+{
+	if (game_grid[res_row][0] == current_player &&
+	    game_grid[res_row][1] == current_player &&
+	    game_grid[res_row][2] == current_player)
+	{
+		return true;
+	}
+	else if (game_grid[0][res_column] == current_player &&
+		 game_grid[1][res_column] == current_player &&
+		 game_grid[2][res_column] == current_player)
+	{
+		return true;
+	}
+	else if ((game_grid[0][0] == current_player &&
+		  game_grid[1][1] == current_player &&
+		  game_grid[2][2] == current_player) ||
+		 (game_grid[0][2] == current_player &&
+		  game_grid[1][1] == current_player &&
+		  game_grid[2][0] == current_player))
+	{
+		return true;
+	};
+	
+	return false;
+};
+
+
 
 
 int main()
 {
 	bool game_over;
+	bool game_win;
+	bool space_ok;
+
 	char game_grid[3][3];
 	char player_1;
 	char player_2;
 	char current_player;
+	char buffer_clear;
+
 	int res_row;
 	int res_column;
-	int space_status;
 	int turn_count;
 	
 	for (int i=0; i<3; i++)
@@ -79,26 +111,52 @@ int main()
 	{
 		draw_board(game_grid);
 		
-		printf("%c, %s\n", current_player, "Please input the coordinates of the space you'd like to fill (Format - row, column):");
-		scanf("%d, %d", &res_row, &res_column);
+		printf("\n%c, %s\n", current_player, "Please input the coordinates of the space you'd like to fill (Format - row, column):");
+
+		if (scanf("%d, %d", &res_row, &res_column) == 0)
+		{	
+			printf("\n%s\n", "Invalid input, try again");
+
+			do
+			{
+				buffer_clear = getchar();
+			}
+			while (!isdigit(buffer_clear));
+			ungetc(buffer_clear, stdin);	
+		};
+
 		if (res_row < 1 ||
 		    res_row > 3 ||
 		    res_column < 1 ||
 		    res_column > 3)
 		{
-			printf("%s\n", "Invalid input, try again");
+			printf("\n%s\n", "Invalid input, please check coordinates.");
 			continue;
 		};
+		
+		res_row -= 1;
+		res_column -= 1;
+		
+		space_ok = check_space(game_grid, res_row, res_column);	
 
-
-		space_status = check_space(game_grid, res_row, res_column);	
-
-		if (!(space_status))
+		if (space_ok)
 		{
 			turn_count += 1;
-			game_grid[res_row-1][res_column-1] = current_player;
-			
-			if (current_player == player_1)
+			game_grid[res_row][res_column] = current_player;
+			game_win = check_win(game_grid, res_row, res_column, current_player);
+
+			if (game_win)
+			{
+				printf("\n%c %s\n", current_player, "wins!");
+				game_over = true;
+
+			}	
+			else if (turn_count == 9)
+			{
+				printf("\n%s\n", "It's a draw!");
+				game_over = true;
+			}
+			else if (current_player == player_1)
 			{
 				current_player = player_2;
 			}
@@ -108,13 +166,12 @@ int main()
 			}
 			else
 			{
-				printf("%s", "LINE 110 FUCKED");
+				printf("\n%s\n", "LINE 110 FUCKED");
 			};	
-
-			if (turn_count == 9)
-			{
-				game_over = true;
-			};
+		}
+		else
+		{
+			printf("\n%s\n", "Space filled, please try again.");
 		};
 	};
 
